988c2b348b821486c8853da846779a2582d4ea39
josnw <66671217+josnw@users.noreply.github.com>
2023-10-11 10:12:24 +0200

    add web config edit fpr first admin

---
+++ intern/views/settings_admin_view.php
@@ -0,0 +1,10 @@
+<?php if ($_SESSION["level"] < 9) { exit; }?>
+<form action="#" method="POST" enctype="multipart/form-data" >
+	<div class="DSEdit">
+		<div class="DSFeld2">Config PHP<br>
+		Admin Passwort: <input name="password" type="password"><br/><br/>
+		<textarea name=config style="width:820px; height:400px;"><?php print file_get_contents('./intern/config.php')?> </textarea> 
+		</div>
+		<div class="DSFeld2 right" style="background: #AA5555;"><input type="submit" name="setConfig" value="Speichern"></div>
+	</div>
+</form>
