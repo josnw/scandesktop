0a5860dfa51a44dfa5d87da947677dc061e187d6
josnw <66671217+josnw@users.noreply.github.com>
2024-11-04 08:19:43 +0100

    Check local jsquery

--- intern/views/header.php
+++ intern/views/header.php
@@ -5,9 +5,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="./css/master.css" media=screen>
 <link rel="stylesheet" type="text/css" href="./css/masterprint.css" media=print>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+<script src="<?php (file_exists("./js/jquery-1.12.4.min.js") ? print "./js" : print "https://code.jquery.com") ?>/jquery-1.12.4.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php (file_exists("./js/jquery-ui.min.js") ? print "./js" : print "https://code.jquery.com/ui/1.12.1") ?>/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php (file_exists("./js/jquery-ui.css") ? print "./js" : print "https://code.jquery.com/ui/1.12.1/themes/base") ?>/jquery-ui.css"/>
 
 <script src="./js/scripts.js" type="text/javascript"></script>
 	
